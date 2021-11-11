f828c9c05790056263c63310ee57aac8b02d5c3b
Burak Kural <brk-krl23@outlook.com>
2021-11-11 22:03:15 +0300

    Add project files.

---
+++ ECommerce.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECommerce.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
